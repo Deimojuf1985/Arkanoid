181af9f77aaeba0d68f47ddb60b65547964d4f41
Juan Fernando Osorio <jufeosgut@gmail.com>
2018-02-03 11:52:41 -0500

    arkanoid
    juego arkanoid del 2do trimestre

---
+++ ARKAnoidJ/Assets/code/Mundo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mundo : MonoBehaviour
+{
+	public	float distanciahorizontal;
+	public float distanciavertical;
+	public TextAsset mundo_1; 
+	public GameObject ladrillo;
+	public int cant_i;
+	public int cant_j;
+
+	void Start () 
+	{
+	
+		for (int i = 0; i < cant_i; i++) 
+		{
+			
+			for (int j = 0; j < cant_j; j++)
+			{
+				GameObject go = Instantiate(ladrillo,this.transform);
+				go.transform.localPosition = new Vector2 (i*distanciavertical,j*distanciahorizontal);
+			}
+		}
+	}
+	void Update () 
+	{
+		
+	}
+}
+ 
---
+++ ARKAnoidJ/Assets/code/Puntajje.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Puntajje : MonoBehaviour 
+{
+	public int vp;
+	public int vp2;
+	public int vp3;
+	public int vp4;
+	public int puntosvida = 3;
+	public Text vidas;
+    public static int puntos = 0 ;
+	public Text play1;
+ 
+    public void puntop1 ()
+	{
+		General.puntos = General.puntos + vp;
+	 	play1.text = "Punto: " + General.puntos.ToString();
+        if (General.puntos == 100)
+        {
+            puntosvida = puntosvida + 1;
+        }
+            if (General.puntos == 1600) 
+		{
+			SceneManager.LoadScene ("Nivel_2");
+		}
+		if (General.puntos == 3000) 
+		{
+			SceneManager.LoadScene ("Nivel_3");
+		}
+		if (General.puntos == 12400) 
+		{
+			SceneManager.LoadScene ("MENU");
+		}
+	}
+
+    public void puntop2 ()
+	{
+		General.puntos = General.puntos + vp2;
+		play1.text = "Punto: " + General.puntos.ToString();
+        if (General.puntos == 100)
+        {
+            puntosvida = puntosvida + 1;
+        }
+        if (General.puntos == 1600) 
+		{
+			SceneManager.LoadScene ("Nivel_2");
+		}
+		if (General.puntos == 3000) 
+		{
+			SceneManager.LoadScene ("Nivel_3");
+		}
+		if (General.puntos == 9000) 
+		{
+			SceneManager.LoadScene ("MENU");
+		}
+	}
+
+	public void puntop3 ()
+	{
+		General.puntos = General.puntos + vp3;
+		play1.text = "Punto: " + General.puntos.ToString();
+        if (General.puntos == 100)
+        {
+            puntosvida = puntosvida + 1;
+        }
+
+        if (General.puntos == 1600) 
+		{
+			SceneManager.LoadScene ("Nivel_2");
+		}
+		if (General.puntos == 3000) 
+		{
+			SceneManager.LoadScene ("Nivel_3");
+		}
+		if (General.puntos == 9000) 
+		{
+			SceneManager.LoadScene ("MENU");
+		}
+	}
+
+	public void puntop4 ()
+	{
+		General.puntos = General.puntos + vp4;
+		play1.text = "Punto: " + General.puntos.ToString();
+        if (General.puntos == 100)
+        {
+            puntosvida = puntosvida + 1;
+        }
+        if (General.puntos == 1600) 
+		{
+			SceneManager.LoadScene ("Nivel_2");
+		}
+		if (General.puntos == 3000) 
+		{
+			SceneManager.LoadScene ("Nivel_3");
+		}
+		if (General.puntos == 9000) 
+		{
+			SceneManager.LoadScene ("MENU");
+		}
+
+    }
+
+public void puntovida ()
+
+	{
+		puntosvida = puntosvida - 1;
+		vidas.text = puntosvida.ToString ();
+		if (puntosvida <= 0)
+		{
+			SceneManager.LoadScene ("GameOver");
+		}
+
+        
+    }
+}
+
